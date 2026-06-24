d590cf14a6edd1b2547b692a2e9b1944cf331924
MarkusLoew <loew@mytum.de>
2026-06-25 09:28:48 +1000

    updated doc

--- R/CampbellLogger-package.R
+++ R/CampbellLogger-package.R
@@ -2,6 +2,6 @@
 
 #' @docType package
 #' @name CampbellLogger
-#' @description Import single or multiple Campbell logger *.dat files. Provides functions to merge data from sensors of the same type, calculate sunrise and sunset times for the research site, and provides functions to create ggplot figures from sensor data.
+#' @description Import single or multiple Campbell logger *.dat files.
 
 utils::globalVariables(c("SYSTEM", "variable", "Date"))
